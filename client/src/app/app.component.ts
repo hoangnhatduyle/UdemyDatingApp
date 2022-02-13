cab34c86a50f773151c7bf28e64c514c10189430
Hoang Nhat Duy Le <lehoangnhatduy2000@gmail.com>
2022-02-13 12:07:31 -0500

    Set up Angular Components and views

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({ //decorator
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit {
+  title = 'Dating App';
+  users: any; //user can be any type (strings, numbers, ...)
+
+  constructor(private http: HttpClient) { //dependency injection
+
+  }
+  ngOnInit() {
+    this.getUsers();
+  }
+
+  getUsers() {
+    this.http.get('https://localhost:5001/api/users').subscribe(response => {
+      this.users = response;
+    }, error => {
+      console.log(error);
+    })
+    //get method returns observable - way of Angular to handle asynchronous code  
+  }
 }
