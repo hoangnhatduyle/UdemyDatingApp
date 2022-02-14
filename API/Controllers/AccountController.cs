934f7514dd18c0957afd300d9d3fadacd664d120
Hoang Nhat Duy Le <hndle@nysus.net>
2022-02-14 16:58:21 -0500

    add login endpoint

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -33,6 +33,23 @@ namespace API.Controllers
             return user;
         }
 
+        [HttpPost("login")]
+        public async Task<ActionResult<AppUser>> Login(LoginDto loginDtio) {
+            var user = await _context.Users.SingleOrDefaultAsync(user => user.UserName == loginDtio.UserName);
+
+            if (user == null) return Unauthorized("Invalid Username");
+
+            using var hmac = new HMACSHA512(user.PasswordSalt);
+
+            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDtio.Password));
+
+            for (int i = 0; i < computedHash.Length; i++) {
+                if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid Password");
+            }
+
+            return user;
+        }
+
         private async Task<bool> UserExists(string username) 
         {
             return await _context.Users.AnyAsync(user => user.UserName == username.ToLower());
