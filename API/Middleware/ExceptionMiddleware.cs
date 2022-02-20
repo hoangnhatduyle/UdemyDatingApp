c048bdc4cc2894127fc81450464eab18ed9dcef9
Hoang Nhat Duy Le <lehoangnhatduy2000@gmail.com>
2022-02-19 19:23:56 -0500

    Add Error Handling

---
+++ API/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,48 @@
+using System.Net;
+using System.Text.Json;
+using API.Errors;
+
+namespace API.Middleware
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionMiddleware> _logger;
+        private readonly IHostEnvironment _env;
+        public ExceptionMiddleware(RequestDelegate next, 
+                                   ILogger<ExceptionMiddleware> logger, 
+                                   IHostEnvironment env)
+        {
+            _env = env;
+            _logger = logger;
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);       //get the HTTP Request and pass to this middleware
+            }
+            catch (Exception ex)
+            {
+                //A HttpContext object holds information about the current HTTP request
+                _logger.LogError(ex, ex.Message);       //log the error to the terminal
+                context.Response.ContentType = "application/json";
+                context.Response.StatusCode = (int) HttpStatusCode.InternalServerError;
+
+                var response = _env.IsDevelopment() 
+                ? new ApiException(context.Response.StatusCode, ex.Message, ex.StackTrace?.ToString()) 
+                : new ApiException(context.Response.StatusCode, "Internal Server Error");
+
+                var options = new JsonSerializerOptions{
+                    PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+                };
+
+                var json = JsonSerializer.Serialize(response, options);
+
+                await context.Response.WriteAsync(json);
+            }
+        }
+    }
+}
