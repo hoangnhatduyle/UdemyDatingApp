aa70a735107320916be6b31e53154040edd558d5
Hoang Nhat Duy Le <lehoangnhatduy2000@gmail.com>
2022-03-02 21:56:14 -0500

    Start Pagination

--- API/Data/UserRepository.cs
+++ API/Data/UserRepository.cs
@@ -1,5 +1,6 @@
 using API.DTOs;
 using API.Entities;
+using API.Helpers;
 using API.Interfaces;
 using AutoMapper;
 using AutoMapper.QueryableExtensions;
@@ -24,11 +25,10 @@ namespace API.Data
                 .SingleOrDefaultAsync();
         }
 
-        public async Task<IEnumerable<MemberDto>> GetMembersAsync()
+        public async Task<PagedList<MemberDto>> GetMembersAsync(UserParams userParams)
         {
-            return await _context.Users
-                .ProjectTo<MemberDto>(_mapper.ConfigurationProvider)
-                .ToListAsync();
+            var query =  _context.Users
+                .ProjectTo<MemberDto>(_mapper.ConfigurationProvider);
         }
 
         public async Task<AppUser> GetUserByIdAsync(int id)
